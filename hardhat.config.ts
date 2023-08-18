c35a1488b2aeae8d3ceaa3eeba677f792dcdff5a
Stefan Eroteev <eroteev@gmail.com>
2023-08-18 14:15:53 +0300

    Implement lime token basic functionality

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,30 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
+const lazyImport = async(module: any) => {
+  return await import(module);
+}
+
+task("deploy", "Deploys contract").setAction(async() => {
+  const { main } = await lazyImport("./scripts/deploy");
+  await main();
+})
+
 const config: HardhatUserConfig = {
-  solidity: "0.8.19",
+  solidity: {
+    version: "0.8.19",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  },
+  networks: {
+    localhost: {
+      url: 'http://127.0.0.1:8545/'
+    }
+  }
 };
 
 export default config;
